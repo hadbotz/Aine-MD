690cfce80233ca2dac04db021a7ee85785db0c25
HAD <103582402+hadbotz@users.noreply.github.com>
2022-06-24 13:36:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6285749433476'],
+  ['628128551181'],
+  ['6285749433476', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6285749433476'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -56,9 +56,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
-global.wm = '© Aine'
+global.packname = sticker_NYOLONG
+global.author = sticker_HAD STORE
+global.wm = '© HadBotz'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
